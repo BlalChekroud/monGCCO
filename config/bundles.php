046a66e405bfd1c6e463aefa257b56cf3945e24a
Blal <bleil.blal-chekroud@ugb.edu.sn>
2025-02-11 09:56:54 +0000

    After install yarn and uninstall ux-turbo

--- config/bundles.php
+++ config/bundles.php
@@ -19,5 +19,4 @@ return [
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
